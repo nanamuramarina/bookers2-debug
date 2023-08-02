4b18bb3d622734c646bf7b45168f3dcc0ab8fb53
EC2 Default User <ec2-user@ip-172-31-3-23.ap-northeast-1.compute.internal>
2023-08-02 08:19:21 +0000

    編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,17 @@ class UsersController < ApplicationController
 
   def destroy
   end
+  
+  def follows
+    user = User.find(params[:id])
+    @users = user.following_users
+  end
+  
+  def followers
+    user = User.find(params[:id])
+    @user = user.follower_users
+  end
+
 
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,13 @@
 class Book < ApplicationRecord
   has_one_attached :image
+  has_many :favorites, dependent: :destroy
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
   validates :title, presence: true
   validates :body, length:{maximum:200}, presence:true
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
 
